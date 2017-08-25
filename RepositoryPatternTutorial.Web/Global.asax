15074e500c2002b334e691eba62c21a2f479a441
ansab.iqbal <ansab.iqbal@SID-257>
2017-08-25 10:21:53 +0500

    Add project files.

---
+++ RepositoryPatternTutorial.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RepositoryPatternTutorial.Web.MvcApplication" Language="C#" %>
---
+++ RepositoryPatternTutorial.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RepositoryPatternTutorial.Web.Startup))]
+namespace RepositoryPatternTutorial.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
